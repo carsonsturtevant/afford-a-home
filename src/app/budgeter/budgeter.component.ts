2da7899f250a73a2269f4bb80dff7ba1b52ef697
carsonsturtevant <carson.sturtevant@gmail.com>
2019-11-24 00:40:08 -0700

    started creating budgeter

--- src/app/budgeter/budgeter.component.ts
+++ src/app/budgeter/budgeter.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgeterComponent implements OnInit {
 
+  homePrice: number;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onKeyHomePrice(value: number) {
+    this.homePrice = value;
   }
 
 }
